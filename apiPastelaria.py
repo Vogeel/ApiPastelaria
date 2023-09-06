82f3b2bf328579154bae0ebb2e02066b1cf3f13b
vitor vogel <vitor-vogel@hotmail.com>
2023-09-06 14:18:49 -0300

    feat: CRUD de funcionário feita para SQLite

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -17,6 +17,9 @@ app.include_router(ProdutoDAO.router)
 def root():
     return {"detail":"API Pastelaria", "Swagger UI": "http://127.0.0.1:8000/docs", "ReDoc": "http://127.0.0.1:8000/redoc" }
 
+import db
+db.criaTabelas()
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run('apiPastelaria:app', host=HOST, port=int(PORT), reload=RELOAD)
--- mod_funcionario/FuncionarioDAO.py
+++ mod_funcionario/FuncionarioDAO.py
@@ -1,25 +1,75 @@
 from fastapi import APIRouter
 from mod_funcionario.Funcionario import Funcionario
+import db
+from mod_funcionario.FuncionarioModel import FuncionarioDB
 
 router = APIRouter()
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 @router.get("/funcionario/", tags=["Funcionário"])
 def get_funcionario(f: Funcionario):
-    return {"msg": "get todos executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 200
+     return {"msg": "get todos executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 200
 
 @router.get("/funcionario/{id}", tags=["Funcionário"])
-def get_funcionario(id: int, f: Funcionario):
-    return {"msg": "get um executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 200
+def get_funcionario(id: int):
+    try:
+        session = db.Session()
+        # busca um com filtro
+        dados = session.query(FuncionarioDB).filter(FuncionarioDB.id_funcionario == id).all()
+        return dados, 200
+    except Exception as e:
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
 
 @router.post("/funcionario/", tags=["Funcionário"])
-def post_funcionario(f: Funcionario):
-    return {"msg": "post executado", "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 200
+def post_funcionario(corpo: Funcionario):
+    try:
+        session = db.Session()
+        dados = FuncionarioDB(None, corpo.nome, corpo.matricula,
+        corpo.cpf, corpo.telefone, corpo.grupo, corpo.senha)
+        session.add(dados)
+        session.commit()
+        return {"id": dados.id_funcionario}, 200
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+            session.close()
 
 @router.put("/funcionario/{id}", tags=["Funcionário"])
-def put_funcionario(id: int, f: Funcionario):
-    return {"msg": "put executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 201
+def put_funcionario(id: int, corpo: Funcionario):
+    try:
+        session = db.Session()
+        dados = session.query(FuncionarioDB).filter(
+        FuncionarioDB.id_funcionario == id).one()
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.telefone = corpo.telefone
+        dados.senha = corpo.senha
+        dados.matricula = corpo.matricula
+        dados.grupo = corpo.grupo
+        session.add(dados)
+        session.commit()
+        return {"id": dados.id_funcionario}, 200
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
 
 @router.delete("/funcionario/{id}", tags=["Funcionário"])
-def delete_funcionario(id: int, f:Funcionario):
-    return {"msg": "delete executado", "id": id, "nome": f.nome, "cpf": f.cpf, "telefone": f.telefone}, 201
+def delete_funcionario(id: int):
+    try:
+        session = db.Session()
+        dados = session.query(FuncionarioDB).filter(FuncionarioDB.id_funcionario == id).one()
+        session.delete(dados)
+        session.commit()
+        return {"id": dados.id_funcionario}, 200
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+
