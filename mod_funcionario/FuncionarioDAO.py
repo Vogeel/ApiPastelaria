06c7cb7a589ec9b7988a94f91a30aad4ea80bb4d
vitor vogel <vitor-vogel@hotmail.com>
2023-09-12 20:06:37 -0300

    feat: TOekn e key validados

--- mod_funcionario/FuncionarioDAO.py
+++ mod_funcionario/FuncionarioDAO.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 from mod_funcionario.Funcionario import Funcionario
 import db
 from mod_funcionario.FuncionarioModel import FuncionarioDB
+# import da segurança
+from fastapi import Depends
+import security
 
-router = APIRouter()
+router = APIRouter( dependencies=[Depends(security.verify_token), Depends(security.verify_key)] )
+
 # Criar as rotas/endpoints: GET, POST, PUT, DELETE
 
 @router.get("/funcionario/", tags=["Funcionário"])
